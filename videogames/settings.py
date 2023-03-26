42f09b1f8a323e95e79da1fc0411950030cd88c7
AndresCarlosamaDev <andrescarlosama97@gmail.com>
2023-03-26 11:03:12 -0500

    Commit setting

--- videogames/settings.py
+++ videogames/settings.py
@@ -95,8 +95,10 @@ WSGI_APPLICATION = 'videogames.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs
-        default='postgresql://postgres:postgres@localhost:5432/videogames',
-        conn_max_age=600    )}
+                # 'postgres://videogames_6ozi_user:j99d8pxlP7YeufwDdUopCynsXHN88zkW@dpg-cgfm0l64daddcg0i264g-a/videogames_6ozi'
+        default='postgres://videogames_6ozi_user:j99d8pxlP7YeufwDdUopCynsXHN88zkW@dpg-cgfm0l64daddcg0i264g-a/videogames_6ozi',
+        conn_max_age=600
+        )}
 
 
 # Password validation
